9d67864b2c52c9f5c4a45bea0a0f4be7b6906fef
markus6890 <markushygedombrowski@gmail.com>
2024-04-13 18:55:47 +0200

    added randomInt and actionbar message

--- src/main/java/com/gmail/markushygedombrowski/utils/Utils.java
+++ src/main/java/com/gmail/markushygedombrowski/utils/Utils.java
@@ -11,6 +11,7 @@ import com.sk89q.worldguard.protection.flags.StateFlag;
 import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
 import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import net.md_5.bungee.api.ChatMessageType;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -18,6 +19,9 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.util.BlockIterator;
 
+import java.awt.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.text.DecimalFormat;
 import java.util.*;
 
@@ -125,6 +129,44 @@ public class Utils {
         return num <= pro;
 
     }
+
+    public static int randomInt(int min, int max) {
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
+    public static void sendActionbar(Player player, String message) {
+        if (player == null || message == null) return;
+        String nmsVersion = Bukkit.getServer().getClass().getPackage().getName();
+        nmsVersion = nmsVersion.substring(nmsVersion.lastIndexOf(".") + 1);
+
+        //1.8.x and 1.9.x
+        try {
+            Class<?> craftPlayerClass = Class.forName("org.bukkit.craftbukkit." + nmsVersion + ".entity.CraftPlayer");
+            Object craftPlayer = craftPlayerClass.cast(player);
+
+            Class<?> ppoc = Class.forName("net.minecraft.server." + nmsVersion + ".PacketPlayOutChat");
+            Class<?> packet = Class.forName("net.minecraft.server." + nmsVersion + ".Packet");
+            Object packetPlayOutChat;
+            Class<?> chat = Class.forName("net.minecraft.server." + nmsVersion + (nmsVersion.equalsIgnoreCase("v1_8_R1") ? ".ChatSerializer" : ".ChatComponentText"));
+            Class<?> chatBaseComponent = Class.forName("net.minecraft.server." + nmsVersion + ".IChatBaseComponent");
+
+            Method method = null;
+            if (nmsVersion.equalsIgnoreCase("v1_8_R1")) method = chat.getDeclaredMethod("a", String.class);
+
+            Object object = nmsVersion.equalsIgnoreCase("v1_8_R1") ? chatBaseComponent.cast(method.invoke(chat, "{'text': '" + message + "'}")) : chat.getConstructor(new Class[]{String.class}).newInstance(message);
+            packetPlayOutChat = ppoc.getConstructor(new Class[]{chatBaseComponent, Byte.TYPE}).newInstance(object, (byte) 2);
+
+            Method handle = craftPlayerClass.getDeclaredMethod("getHandle");
+            Object iCraftPlayer = handle.invoke(craftPlayer);
+            Field playerConnectionField = iCraftPlayer.getClass().getDeclaredField("playerConnection");
+            Object playerConnection = playerConnectionField.get(iCraftPlayer);
+            Method sendPacket = playerConnection.getClass().getDeclaredMethod("sendPacket", packet);
+            sendPacket.invoke(playerConnection, packetPlayOutChat);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
 
 
 
